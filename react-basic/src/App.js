ce0376bff613af8d982a08bb6a615309cba34db3
hcartist <hcartist@naver.com>
2023-07-23 17:03:12 +0900

    07/23

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -334,10 +334,11 @@ props
 // function Beer({beer}) {
 //   console.log(beer) // 전달받은 변수에 접근한다
 // return (
+
 //   <ul>
 //     <li>이름: {beer.name}</li>
-//     <li>원산지: {beer.origin}</li>
-//     <li>판매중: {beer.available ? "예" : "아니오"}</li>
+//     <li>이름: {beer.origin}</li>
+//     <li>이름: {beer.available ? "예" : "아니오"}</li>
 //   </ul>
 //   )
 // }
@@ -345,164 +346,162 @@ props
 // function Snippet() {
 
 //   // 지역 변수(선언된 함수 안에서만 접근할 수 있다)
-//   const irishBeer = {
-//   name: "Guinness",
-//   origin: "Ireland",
-//   available: false
-//   };
+// const irishBeer = {
+// name: "Guinness",
+// origin: "Ireland",
+// available: false
+// };
+
+// return (
+//   <>
+//   <h2>Beer</h2>
+//   {/* Beer컴포넌트에게  irishBeer변수를 전달한다 */}
+//   <Beer beer={irishBeer}/>
+//   </>
+//   )
+// }
+
+
+
+
+// function Snippet() {
+//   // 지역변수
+//   const DATA = {
+//     video: {
+//       id: 'v0', 
+//       title: '고양이는 액체일까?',
+//       source: "https://mblogthumb-phinf.pstatic.net/MjAxOTA3MDRfMTM2/MDAxNTYyMjE2Mzc1NjQ3.ywQ3_FfZUTmg8oMQSIc3HecxkqJ1vzwq4Pwcu6diyOQg.hkGxwu96hY8E5HZDZnJCL4yXXaITk4-AJhhg8W6u2Ywg.JPEG.with_msip/2-1.jpg?type=w800"
+//     },
+//     comments: [
+//       { id: 'c0', content: '1빠' },
+//       { id: 'c1', content: '2빠' },
+//       { id: 'c2', content: '유치하게 등수는... 3빠' },
+//     ],
+//     suggestedVideos: [
+//       { id: 's0', title: '고양이는 정말 폭력적일까?' },
+//       { id: 's1', title: '고양이는 정말 자기가 신이라고 생각할까?' },
+//       { id: 's3', title: '냥냥펀치는 얼마나 아플까?' }
+//     ]
+//   }
+
 
 //   return (
 //     <>
-//     <h2>Beer</h2>
-//     {/* Beer컴포넌트에게 irishBeer 변수를 전달한다*/}
-//     <Beer beer={irishBeer}/>
+//     <header>
+//       <h1>Youtube</h1>
+//     </header>
+
+//     <main>
+//     <Content video={DATA.video}/>
+
+//     <h2>comments</h2>
+//     <Comments comments={DATA.comments}/>
+//     </main>
+
+// <aside>
+//     <h2>suggested videos</h2>
+//     <Suggested suggestedVideos={DATA.suggestedVideos} />
+//     </aside>
+//     </>
+//     ) 
+// }
+
+
+
+// function Content({ video }) {
+//   console.log(video)
+//   return (
+//     <>
+//       <h2>{video.title}</h2>
+//       <img src={video.source} alt={video.title} width="100%" />
+//     </>
+//   )
+// }
+
+// function Comments({comments}) {
+
+//   const commentList = comments.map(comment => (
+//     <li key={comment.id}>{comment.content}</li>
+//     ))
+
+// return (
+// <ul>
+//   {commentList}
+// </ul>
+//   )
+// }
+
+// function Suggested({suggestedVideos}) {
+//   console.log(suggestedVideos)
+
+//   const suggestedVideoList = suggestedVideos.map(video => (
+//     <li key={video.id}>{video.title}</li>  
+//   ))
+// return (
+//   <>
+// <ul>
+//   {suggestedVideoList}
+// </ul>
+//   </>
+//   ) 
+// }
+
+
+
+
+/*
+childrn props
+컴포넌트를 트리형태로 만들 수 있다
+*/
+
+// function Layout ({children}) {
+// // children이란 이름이 정해져있어서 꼭 children이라고 써야됌
+//   return (
+//     <>
+//     <h1>Instagram</h1>
+//     <nav>
+//       <ul>
+//         <li>Home</li>
+//         <li>Posts</li>
+//         <li>Profiles</li>
+//       </ul>
+//     </nav>
+
+//     <main style={{ padding: "1rem 0"}}>
+//       {/* Article컴포넌트를 나타낸다 */}
+//       {children}
+//     </main>
+
+//     <footer>
+//       <small>2023 &copy; Instagram</small>
+//     </footer>
 //     </>
 //     )
 // }
 
-
-
-/*
-DATA, 컴포넌트 합성, props를 이용하여 왼쪽의 뷰를 완성해보세요
-*/
-// function Snippet() {
-// // 지역변수
-// const DATA = {
-// video: {
-// id: 'v0',
-// title: '고양이는 왜 액체일까?',
-// source: 'https://www.chemicalnews.co.kr/news/photo/202106/3636_10174_4958.jpg'
-// },
-
-// comments: [
-// { id: 'c0', content: '1빠' },
-// { id: 'c1', content: '2빠' },
-// { id: 'c2', content: '유치하게 등수는.. 3빠' },
-// ],
-
-// suggestedVideos: [
-//   { id: 's0', title: '고양이는 정말 폭력적일까?' },
-//   { id: 's1', title: '고양이는 정말 자기가 신이라고 생각할까?' },
-//   { id: 's2', title: '냥냥펀치는 얼마나 아플까?' },
-// ]
-// }
-
-// return (
-//   <>
-// <header>
-//   <h1>Youtube</h1>
-// </header>
-
-// <main>
-//   <Content video={DATA.video}/>
-
-//   <h2>댓글</h2>
-//   <Comments comments={DATA.comments}/>
-// </main>
-
-// <adide>
-//   <h2>추천 영상</h2>
-//   Suggested suggestedVideos={DATA.suggestedVideos}/>
-// </adide>
-// </>
-// )
-// }
-
-
-// // 영상 부분
-// function Content({video}) {
-// console.log(video) // 전달이 잘 되었는지 확인
-
-// return (
-//   <>
-//   <h2>{video.title}</h2>
-//   <img src={video.source} alt={video.title} width='100%' />
-//   </>
-//   )
-// }
-
-// // 댓글
-// function Comments({comments}) {
-// console.log(comments);
-
-// const commentList = comments.map(comment => (
-//   <li key={comment.id}>{comment.content}</li>
-//   ))
+// function Article() {
 
 //   return (
-//     <ul>
-//       {commentList}
-//     </ul>
+//     <>
+//     <img
+//     src="https://external-preview.redd.it/Lpgb3alLNJ0BOp_YH6hj2UaY-N60pmkUHf-gS6hbifE.jpg?auto=webp&s=44a73214eb47bf11c5d5c2dca4d971113c06baf3"
+//     alt=""
+//     width="100%"
+//     />
+//     <p>
+//       <b>snoop_dogg</b>
+//       주인 나가서 한컷.
+//     </p>
+
+//     <small>1일 전</small>
+//     </>
 //     )
-// }
-
-// //추천 영상
-// function Suggested({suggestedVideos}) {
-// console.log(suggestedVideos);
-
-// const suggestedVideoList = suggestedVideos.map(video => (
-//   <li key={video.id}>{video.title}</li>
-//   ))
-
-//   return (
-//     <ul>
-//       {suggestedVideoList}
-//     </ul>
-//     )
-// 
-
-
-
-/*
-children props
-컴포넌트를 트리형태로 만들 수 있다
-*/
-
-// function Layout({children}) {
-// return (
-//   <>
-//   <h1>Instagram</h1>
-//   <nav>
-//     <ul>
-//       <li>Home</li>
-//       <li>Posts</li>
-//       <li>Profile</li>
-//     </ul>
-//   </nav>
-
-//   <main style={{padding: "1rem 0"}}>
-//     {/*Articles 컴포넌트를 나타낸다*/}
-//     {children}
-//   </main>
-
-//   <footer>
-//     <small>2023 &copy; Instagram</small>
-//   </footer>
-//   </>
-//   )
-// }
-
-// function Articles() {
-// return (
-//   <>
-//   <img src='https://www.chemicalnews.co.kr/news/photo/202106/3636_10174_4958.jpg'
-//   alt=''
-//   width='100%'
-//   />
-//   <p>
-//     <b>snoop_dogg</b>
-//     주인 나가서 한컷.
-//   </p>
-
-//   <small>1일 전</small>
-//   </>
-//   )
 // }
 
 // function Snippet() {
 // return (
 //   <Layout>
-//     <Articles/>
+//     <Article/>
 //   </Layout>
 //   )
 // }
@@ -512,7 +511,7 @@ children props
 
 /*
 useContext Hook(리액트가 제공하는 특별한 함수)
-하위 컴포넌트에 대해 데이터를 전달할 수 있다 (트리 구조)
+하위 컴포넌트에 데이터를 전달할 수 있다(트리 구조)
 */
 
 
@@ -522,8 +521,8 @@ useContext Hook(리액트가 제공하는 특별한 함수)
 // // 인증 관리
 // function AuthProvider({ children }) {
 
-//   // 유저 정보 (지역 변수)
-//   const value = { username: "bunny" };
+//   // 유저 정보(지역 변수)
+//   const value = { username: "Bunny" };
 
 //   return (
 //     <AuthContext.Provider value={value}>
@@ -533,10 +532,10 @@ useContext Hook(리액트가 제공하는 특별한 함수)
 // }
 
 // function Layout({ children }) {
-//   // 전달받은 value 객체에 접근
+//   // value객체에 접근(어떤 하위 컴포넌트라도 접근할 수 있다)
 //   const auth = useContext(AuthContext);
 
-//   console.log(auth);
+//   console.log(auth)
 
 //   return (
 //     <>
@@ -545,14 +544,13 @@ useContext Hook(리액트가 제공하는 특별한 함수)
 //         <ul>
 //           <li>Home</li>
 //           <li>Posts</li>
-//           <li>Profile</li>
+//           <li>Profiles</li>
 //         </ul>
 //       </nav>
 
 //       <p>안녕하세요, {auth.username}님!</p>
 
 //       <main style={{ padding: "1rem 0" }}>
-//         {/*Articles 컴포넌트를 나타낸다*/}
 //         {children}
 //       </main>
 
@@ -562,18 +560,19 @@ useContext Hook(리액트가 제공하는 특별한 함수)
 //     </>
 //   )
 // }
- 
+
 // function Article() {
+//   // value객체에 접근(어떤 하위 컴포넌트라도 접근할 수 있다)
+//   const auth = useContext(AuthContext);
 
-// const auth = useContext(AuthContext);
-
-// console.log(auth);
+//   console.log(auth);
 
 //   return (
 //     <>
-//       <img src='https://www.chemicalnews.co.kr/news/photo/202106/3636_10174_4958.jpg'
-//         alt=''
-//         width='100%'
+//       <img
+//         src="https://external-preview.redd.it/Lpgb3alLNJ0BOp_YH6hj2UaY-N60pmkUHf-gS6hbifE.jpg?auto=webp&s=44a73214eb47bf11c5d5c2dca4d971113c06baf3"
+//         alt=""
+//         width="100%"
 //       />
 //       <p>
 //         <b>snoop_dogg</b>
@@ -589,7 +588,7 @@ useContext Hook(리액트가 제공하는 특별한 함수)
 //   return (
 //     <AuthProvider>
 //       <Layout>
-//         <Articles />
+//         <Article />
 //       </Layout>
 //     </AuthProvider>
 //   )
@@ -598,12 +597,15 @@ useContext Hook(리액트가 제공하는 특별한 함수)
 
 
 /*
-이벤트 처리
+  이벤트 처리
 */
 
+
+
 // function Snippet() {
+
 // function handleClick() {
-// alert('lol');
+// alert("lol");
 // }
 
 // // onEventName={eventHandler}
@@ -618,28 +620,44 @@ useContext Hook(리액트가 제공하는 특별한 함수)
 
 
 
-
 /*
-뷰 업데이트
+ 뷰 업데이트
 
-useState 사용 방법
-const [state, setState] = useState(initialValue)
+ useState Hook 사용방법
+ 
+ const [state, setState] = useState(initialValue)
 
-state: 컴포넌트 내의 변수 (상태)
-setState(newState): state를 업데이트하는 메서드
-initialValue: state의 초기값(최초 렌더링 시)
-*/
+ state: 컴포넌트 내의 변수(상태)
+ setState(newState): state를 업데이트하는 메서드
+ initialValue: state의 초기값(최초 렌더링 시)
+ */
 
 
 
 // function Snippet() {
-// const [count, setCount] = useState(0);
+//   // useState도 Hook임
+//   const [count, setCount] = useState(0);
 
-// return (
-//   <>
-//   <p>count: {count}</p>
-//   <button onClick={() => setCount(count + 1)}>+</button>
-//   </>
+//   return (
+//     <>
+//       <p>count: {count}</p>
+//       <button onClick={() => setCount(count + 1)}>+</button>
+//     </>
+//   )
+// }
+
+
+
+// function Snippet() {
+
+//   function handleClick() {
+
+//   }
+//   return (
+//     <>
+//       <h1>Subscribe Button</h1>
+//       <button onClick={()=>}>구독하기</button>
+//     </>
 //   )
 // }
 
