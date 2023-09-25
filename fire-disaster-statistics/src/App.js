9093662c459351609c010c7557a33d7ad619a134
hcartist <83622084+hcartist@users.noreply.github.com>
2023-09-25 17:28:20 +0900

    "09/25"

--- fire-disaster-statistics/src/App.js
+++ fire-disaster-statistics/src/App.js
@@ -2,8 +2,24 @@ import { getPublicData } from "./service/api";
 import { useState, useEffect } from "react";
 import Dashboard from "./Components/Dashboard";
 
+const initialData = JSON.parse(localStorage.getItem("data") || "[]"); // 초기 데이터 저장 형태
+
+function saveDoc(data) {
+  localStorage.setItem("data", JSON.stringify(data));
+} // 로컬스토리지에 저장하는 방식
 
 export default function App() {
+
+  const [data, setData] = useState(initialData);
+
+  console.log(data);
+
+  // const FILTER_MAP = {
+  //   seoul_Btn: () => true,
+
+  // }
+
+  
 
   return (
     <>
