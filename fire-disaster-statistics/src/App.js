40710dc37bf428e247478afb2e72d77d74310118
hcartist <83622084+hcartist@users.noreply.github.com>
2023-09-26 16:29:53 +0900

    "09/26"

--- fire-disaster-statistics/src/App.js
+++ fire-disaster-statistics/src/App.js
@@ -1,47 +1,43 @@
 import { getPublicData } from "./service/api";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Dashboard from "./Components/Dashboard";
 import FilterButton from "./Components/FilterButton";
 
-const initialData = JSON.parse(localStorage.getItem("data") || "[]"); // 초기 데이터 저장 형태
+const initialData = JSON.parse(localStorage.getItem("data") || "[]");
 
 function saveDoc(data) {
   localStorage.setItem("data", JSON.stringify(data));
-} // 로컬스토리지에 저장하는 방식
+}
 
 export default function App() {
-
-  const seoul = getPublicData("서울특별시")
-
-  console.log(seoul);
 
   const [data, setData] = useState(initialData);
   const [filter, setFilter] = useState("서울특별시")
 
-  // const [filter, setFilter] = useState("seoul_Btn");
-
   console.log(data);
 
-const FilterButton = filter.map(filter => (
-  <>
-  key={filter.id}
-  name={filter.name}
-  </>
-))
 
+
+  const FILTER_MAP = {
+    seoul_Btn: () => (filterButton.name)
+  }
+
+  
+  
+  
+  const filterButtons = FILTER_MAP.map(filterButton => (
+    <filterButton
+      key={filterButton.id}
+      id={filterButton.id}
+      name={filterButton.name}
+    />
+  ))
 
   return (
-    <>
-      <button className="text-white">
-        서울
-      </button>
-      <button className="text-white">
-        인천
-      </button>
-      <button className="text-white">
-        광주
-      </button>
-      <Dashboard />
-    </>
+    <div>
+      <FilterButton/>
+    </div>
   )
+
+
 };
