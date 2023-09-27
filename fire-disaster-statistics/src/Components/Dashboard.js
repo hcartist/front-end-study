5fc31f909864383baa6d16283d82c5249290242b
hcartist <83622084+hcartist@users.noreply.github.com>
2023-09-27 15:50:57 +0900

    "09/27"

--- fire-disaster-statistics/src/Components/Dashboard.js
+++ fire-disaster-statistics/src/Components/Dashboard.js
@@ -1,59 +1,66 @@
 import { getPublicData } from "../service/api";
 import { useState, useEffect } from 'react';
-import KakaoMap from './KakaoMap';
-import RechartBar from './RechartBar';
-import RechartPie from './RechartPie';
+// import KakaoMap from './KakaoMap';
+// import RechartBar from './RechartBar';
+// import RechartPie from './RechartPie';
 
 const FILTER_MAP = {
-  전체: () => true,
-  서울특별시: (item) => item.시도 === "서울특별시",
-  경기도: (item) => item.시도 === "경기도",
-  경상도: (item) => item.시도 === "경상북도" || "경상남도",
-  전라도: (item) => item.시도 === "전라도" || "전라북도" || "전라남도",
-  제주도: (item) => item.시도 === "제주특별자치도"
+    서울특별시: (item) => item.시도 === "서울특별시",
+    경기도: (item) => item.시도 === "경기도",
+    경상도: (item) => item.시도 === "경상북도" || "경상남도",
+    전라도: (item) => item.시도 === "전라북도" || "전라남도",
+    제주도: (item) => item.시도 === "제주특별자치도"
 }
+
+function saveDoc(r) {
+    localStorage.setItem("r", JSON.stringify(r));
+}
+
+const initialr = JSON.parse(localStorage.getItem("r") || "[]");
 
 export default function Dashboard({ filter }) {
 
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [accidents, setAccidents] = useState([]);
-  const [accidentCount, setAccidentCount] = useState(0)
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [accidents, setAccidents] = useState(initialr);
+    const [accidentCount, setAccidentCount] = useState(0)
 
-  const r = accidents.filter(FILTER_MAP[filter]);
+    const r = accidents.filter(FILTER_MAP[filter]);
 
-  console.log(r)
+    console.log(r)
 
-  console.log(filter)
+    saveDoc(r)
 
-
-  async function fetchData() {
-    try {
-      setIsLoaded(false);
-      setError(null);
-
-      const data = await getPublicData();
-      console.log(data);
-
-      console.log(data.totalCount)
-
-      setAccidents(data.data);
-      setAccidentCount(data.totalCount);
+    // console.log(filter)
 
 
-    } catch (error) {
-      setError(error)
-    } finally {
-      setIsLoaded(true);
+    async function fetchData() {
+        try {
+            setIsLoaded(false);
+            setError(null);
+
+            const data = await getPublicData();
+            console.log(data);
+
+            console.log(data.totalCount)
+
+            setAccidents(data.data);
+            setAccidentCount(data.totalCount);
+
+
+        } catch (error) {
+            setError(error)
+        } finally {
+            setIsLoaded(true);
+        }
     }
-  }
 
-  useEffect(() => {
-    fetchData();
-  }, [])
+    useEffect(() => {
+        fetchData();
+    }, [])
 
-  return (
-    <KakaoMap accidents={accidents} />
-  )
+    // return (
+    //     <KakaoMap accidents={accidents} />
+    // )
 
 };
