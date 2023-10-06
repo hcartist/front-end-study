48b7de2a100a585129d51e11f19c77bde75697ab
hcartist <83622084+hcartist@users.noreply.github.com>
2023-10-06 10:23:16 +0900

    "10/06"

--- fire-disaster-statistics/src/Constants/Areas.js
+++ fire-disaster-statistics/src/Constants/Areas.js
@@ -1,7 +1,10 @@
 export const Areas = [
     { name: "서울특별시" },
-    { name: "경상도" },
+    { name: "경상북도" },
+    { name: "경상남도" },
     { name: "경기도" },
-    { name: "전라도" },
-    { name: "제주도" }
+    { name: "전라북도" },
+    { name: "전라남도" },
+    { name: "제주도" },
+    { name: "인천"}
 ]
