4316e498350f633a37906415ad09331410584fa6
hcartist <83622084+hcartist@users.noreply.github.com>
2023-10-09 22:47:37 +0900

    "10/09"

--- fire-disaster-statistics/src/Components/RechartBar.js
+++ fire-disaster-statistics/src/Components/RechartBar.js
@@ -10,37 +10,41 @@ import {
   } from 'recharts';
   
 
-export default function RechartBar({ accidents }) {
-    const data = accidents.map(accident => {
+export default function RechartBar({ thisAccidents }) {
+
+    const data = thisAccidents.map(thisAccident => {
         return {
-          name: accident.spot_nm.split(" ").slice(2).join(" "),
-          발생건수: accident.occrrnc_cnt,
-          중상자수: accident.se_dnv_cnt,
-          사망자수: accident.dth_dnv_cnt
+          name: thisAccident.시도 + thisAccident.시군구,
+          재산피해소계: thisAccident.재산피해소계,
+          부상: thisAccident.부상,
+          사망: thisAccident.사망
         }
       })
+      console.log(data)
     
       return (
+        <div className='h-[400px]'>
         <ResponsiveContainer width="100%" height="100%">
           <ComposedChart
             layout="vertical"
-            width={500}
             height={400}
             data={data}
             margin={{
-              top: 40,
-              left: 120,
+              top: 50,
+              left: 100,
+              right: 70
             }}
           >
             <CartesianGrid stroke="#f5f5f5" />
             <XAxis type="number" />
-            <YAxis dataKey="name" type="category" scale="band" />
+            <YAxis dataKey="name" type="category" scale="band"/>
             <Tooltip />
             <Legend />
-            <Bar dataKey="발생건수" barSize={20} fill="#0088fe" />
-            <Bar dataKey="중상자수" barSize={20} fill="#ffbb28" />
-            <Bar dataKey="사망자수" barSize={20} fill="#ff8042" />
+            <Bar dataKey="재산피해소계" barSize={20} fill="#0088fe" />
+            <Bar dataKey="부상" barSize={20} fill="#ffbb28" />
+            <Bar dataKey="사망" barSize={20} fill="#ff8042" />
           </ComposedChart>
         </ResponsiveContainer>
+        </div>
       );
 };
