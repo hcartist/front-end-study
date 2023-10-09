eb1a95868266c04e210e4631304330aac3d99024
hcartist <83622084+hcartist@users.noreply.github.com>
2023-10-09 22:44:09 +0900

    "10/09"

--- fire-disaster-statistics/src/App.js
+++ fire-disaster-statistics/src/App.js
@@ -4,11 +4,12 @@ import { Areas } from "./Constants/Areas";
 import { Years } from "./Constants/Years"
 
 
-export default function App() {
+export default function App( thisAccidents ) {
 
   const [year, setYear] = useState(Years[0].value)
   const [filter, setFilter] = useState("서울특별시")
-
+  
+console.log(thisAccidents)
 
   const areaList = Areas.map(Area => (
     <option key={Area.name} value={Area.name}>
@@ -23,21 +24,34 @@ export default function App() {
   ))
 
   return (
-    <>
-      <div>
-        <div>
-          <select onChange={({ target }) => setYear(target.value)}
+    <div>
+      <header className="p-[1rem] border-b-[1px] border-gray-500">
+
+        <nav className="flex">
+          <img
+            className="w-7 h-9 mt-[1px] mr-[1rem]"
+            src={process.env.PUBLIC_URL + "/images/logo.png"}
+            alt=""
+          />
+
+          <p className="text-white text-4xl flex">화재사고 통계 조회</p>
+
+        </nav>
+
+        <div className="mt-[1rem] flex">
+          <select 
+          className="p-2 bg-gray-400 rounded-md mr-[5px]"
+          onChange={({ target }) => setYear(target.value)}
           >{yearList}</select>
-          <select onChange={({ target }) => setFilter(target.value)}
+          <select 
+          className="p-2 bg-gray-400 rounded-md"
+          onChange={({ target }) => setFilter(target.value)}
           >{areaList}</select>
-          <h1 className="text-white">개가 있습니다.</h1>
-        </div>
-      </div>
-      <Dashboard
-        year={year}
-        filter={filter}
-      />
-    </>
+          </div>
+      </header>
+
+      <Dashboard year={year} filter={filter} />
+    </div>
   )
 
 };
