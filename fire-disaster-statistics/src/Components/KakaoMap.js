e72de891b5db7bfa1582cf349e9692ae7b550010
hcartist <83622084+hcartist@users.noreply.github.com>
2023-10-09 22:47:16 +0900

    "10/09"

--- fire-disaster-statistics/src/Components/KakaoMap.js
+++ fire-disaster-statistics/src/Components/KakaoMap.js
@@ -1,76 +1,60 @@
 import { useState, useEffect } from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map } from 'react-kakao-maps-sdk';
 
 const { kakao } = window;
 
-export default function KakaoMap({ accidents }) {
+export default function KakaoMap({ filter, thisAccidents }) {
 
-        const [info, setInfo] = useState()
-        const [markers, setMarkers] = useState([])
-        const [map, setMap] = useState()
-        const [areas, setAreas] = useState([])
-      
-        useEffect(() => {
+    const [info, setInfo] = useState()
+    const [markers, setMarkers] = useState([])
+    const [map, setMap] = useState()
+    
+    useEffect(() => {
+        if (!map) return
+        const ps = new kakao.maps.services.Places()
 
-          if (!map) return
-          const ps = new kakao.maps.services.Places()
+        console.log(thisAccidents)
+        
 
-          setAreas(accidents);
-          const thisArea = accidents.filter(accident => accident.시도)
-          // const thisAccidents2 = accidents.filter(FILTER_MAP2[filter]); << 참고해봐..
-          console.log(thisArea)
-
-          ps.keywordSearch({areas}, (data, status, _pagination) => {
+        ps.keywordSearch(filter , (data, status, _pagination) => {
             if (status === kakao.maps.services.Status.OK) {
-              // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-              // LatLngBounds 객체에 좌표를 추가합니다
-              const bounds = new kakao.maps.LatLngBounds()
-              let markers = []
-      
-              for (var i = 0; i < data.length; i++) {
-                // @ts-ignore
-                markers.push({
-                  position: {
-                    lat: data[i].y,
-                    lng: data[i].x,
-                  },
-                  content: data[i].place_name,
-                })
-                // @ts-ignore
-                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
-              }
-              setMarkers(markers)
-      
-              // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-              map.setBounds(bounds)
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+                // LatLngBounds 객체에 좌표를 추가합니다
+                const bounds = new kakao.maps.LatLngBounds()
+                let markers = []
+
+                for (var i = 0; i < data.length; i++) {
+                    // @ts-ignore
+                    markers.push({
+                        position: {
+                            lat: data[i].y,
+                            lng: data[i].x,
+                        },
+                        content: data[i].place_name,
+                    })
+                    // @ts-ignore
+                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
+                }
+                setMarkers(markers)
+
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+                map.setBounds(bounds)
             }
-          })
-        }, [map])
-      
-        return (
-          <Map // 로드뷰를 표시할 Container
+        })
+    }, [thisAccidents])
+
+    return (
+        <Map // 로드뷰를 표시할 Container
             center={{
-              lat: 37.566826,
-              lng: 126.9786567,
+                lat: 37.566826,
+                lng: 126.9786567,
             }}
             style={{
-              width: "100%",
-              height: "350px",
+                height: "750px",
             }}
             level={3}
             onCreate={setMap}
-          >
-            {markers.map((marker) => (
-              <MapMarker
-                key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
-                position={marker.position}
-                onClick={() => setInfo(marker)}
-              >
-                {info &&info.content === marker.content && (
-                  <div style={{color:"#000"}}>{marker.content}</div>
-                )}
-              </MapMarker>
-            ))}
-          </Map>
-        )
+        >
+        </Map>
+    )
 };
